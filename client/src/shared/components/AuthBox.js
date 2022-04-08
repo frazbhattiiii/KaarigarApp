641e049e64e9401a967cfb85583896287a7dc859
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2022-04-08 21:33:56 +0500

    Ahmed Fraz's work

--- client/src/shared/components/AuthBox.js
+++ client/src/shared/components/AuthBox.js
@@ -5,24 +5,40 @@ const BoxWrapper = styled('div')({
   width: '100%',
   height: '100vh',
   display: 'flex',
-  alignItems:'center',
-  justifyContent:'center',
-  background:'#5865F2'
+  // alignItems:'flex-end',
+  justifyContent:'flex-end',
+  background:'white'
 })
+
 export default function AuthBox(props) {
+
   return (
     <div>
-    <BoxWrapper>
+    <BoxWrapper    
+     >
+      <img src={props.img} alt=""  
+        style={props.additionalImageStyles ?props.additionalImageStyles :{
+          marginLeft:'-2rem',
+        width:'800px',
+        height:'600px',}}
+        />
+     
+      
       <Box sx={{
-        width:700,
-        height:400,
-        bgcolor:'#36393f',
+        width:680,
+        height:680,
+        bgcolor:'white',
+        marginTop:'50px',
+        marginRight:'60px',
         borderRadius:'5px',
         boxShadow:'0 2px 10px 0 rgb(0 0 0 / 20%)',
         display:'flex',
         flexDirection:"column",
         padding:'25px'
-      }}>
+      }}
+      style={props.additionalStyles ?props.additionalStyles :{}}
+      >
+
         {props.children}
       </Box>
 
--- client/src/shared/utils/validators.js
+++ client/src/shared/utils/validators.js
@@ -4,15 +4,46 @@ export const validateLoginForm =({mail,password})=>{
 
   return isMailValid && isPasswordValid;
 };
+export const validateContactForm =({name,mail})=>{
+  const isMailValid= validateMail(mail);
+  const isName = validateName(name);
 
-export const validateRegisterForm=({mail,username,password})=>{
-  return validateMail(mail) && validatePassword(password) && validateUsername(username);
+
+  return isMailValid && isName;
+}
+
+export const validateRegisterForm=({mail,username,password,name,phone})=>{
+  return validateMail(mail) && validatePassword(password) && validateUsername(username) && 
+  validatePhone(phone) &&
+  validateName(name);
+}
+const validatePhone=(phone)=>{
+  var pattern = new RegExp(/^[0-9\b]+$/);
+
+  if (pattern.test(phone)) {
+    console.log(phone);
+      if(phone.length>7){
+        return true;
+      }
+ 
+
+  }else if(phone.length < 7){
+
+
+    return false;
+
+  }
+
+
 }
 const validatePassword=(password)=>{
   return password.length >=6 && password.length<12;
 }
 const validateUsername=(username)=>{
   return username.length >=3 && username.length<12;
+}
+const validateName=(name)=>{
+  return name.length >=3 && name.length<20;
 }
 export const validateMail=(mail)=>{
  const emailPattern= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
